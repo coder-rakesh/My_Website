6c262f0ff249fbd4b46ec4c905f644ad46cfdef7
rakesh <rycse875@gmail.com>
2017-08-25 18:27:46 +1200

    changed html

--- website/urls.py
+++ website/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
-    url(r'^/Solutions/$', views.solutions_page, name='solutions_page'),
-    url(r'^/Tutorials/$', views.tutorials_page, name='tutorials_page'),
-    url(r'^/About/$', views.about_page, name='about_page'),
-    url(r'^/solution_detail/(?P<pk>\d+)/$', views.solutions_detail, name='solutions_detail'),
-    url(r'^/tutorial_detail/(?P<pk>\d+)/$', views.tutorials_detail, name='tutorials_detail'),
+    url(r'^$', views.home_page, name='home'),
+    url(r'^/solutions/$', views.solutions_page, name='solutions'),
+    url(r'^/tutorials/$', views.tutorials_page, name='tutorials'),
+    url(r'^/about/$', views.about_page, name='about'),
+    url(r'^/solution_detail/(?P<pk>\d+)/$', views.solutions_detail, name='solution_detail'),
+    url(r'^/tutorial_detail/(?P<pk>\d+)/$', views.tutorials_detail, name='tutorial_detail'),
  ]
