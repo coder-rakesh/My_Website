0d4f0566ed4b2830dd8c3331ba11b6ab925279dd
rakesh <rycse875@gmail.com>
2017-10-10 00:33:56 +1200

    modified templates

--- website/urls.py
+++ website/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^/solutions/$', views.solutions_page, name='solutions'),
-    url(r'^/tutorials/$', views.tutorials_page, name='tutorials'),
-    url(r'^/about/$', views.about_page, name='about'),
-    url(r'^/solution_detail/(?P<pk>\d+)/$', views.solutions_detail, name='solution_detail'),
-    url(r'^/tutorial_detail/(?P<pk>\d+)/$', views.tutorials_detail, name='tutorial_detail'),
+    url(r'^solutions/$', views.solutions_page, name='solutions'),
+    url(r'^tutorials/$', views.tutorials_page, name='tutorials'),
+    url(r'^about/$', views.about_page, name='about'),
+    url(r'^solution_detail/(?P<pk>\d+)/$', views.solutions_detail, name='solution_detail'),
+    url(r'^tutorial_detail/(?P<pk>\d+)/$', views.tutorials_detail, name='tutorial_detail'),
  ]
